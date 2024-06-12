f218c87d3b494ccbf7fd555b9bcf79ea097545fa
dillahmwangi <mcharity241@gmail.com>
2024-06-12 08:49:18 +0300

    Installing remixicon and aos for effects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'; 
+import Aos from 'aos';
+
 
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+  })
   return (
   <h1>Hello World</h1>
   );
