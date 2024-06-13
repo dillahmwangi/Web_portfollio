7a101b85941e999155c95c10daed1f5945e4fef1
dillahmwangi <mcharity241@gmail.com>
2024-06-13 08:46:32 +0300

    imported all the pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import Aos from 'aos';
 
 import './App.css';
 
+import Header from './components/Header/Header';
+
+import Footer from './components/Footer/Footer';
+import Hero from './components/UI/Hero';
+
 function App() {
+ 
   useEffect(() => {
     Aos.init();
-  })
-  return (
-  <h1>Hello World</h1>
-  );
+  }, []);
+  
+  return <>
+  <Header/>
+    <main>
+      <Hero/>
+    </main>
+  <Footer/>
+</>
+
 }
 
 export default App;
