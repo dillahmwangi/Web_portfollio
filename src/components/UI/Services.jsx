df48261200e075a40f663504007d65d3165ece55
dillahmwangi <mcharity241@gmail.com>
2024-06-14 09:53:35 +0300

    created service page

---
+++ src/components/UI/Services.jsx
@@ -0,0 +1,180 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+   <section id="services">
+    <div className='container lg:pt-5'>
+        <div className='text-center'>
+            <h2 className='text-headingColor font-[800] text-[2.4rem] mb-5'>
+                What do I Help
+            </h2>
+            <p className='lg:max-w-[600px] lg:mx-auto text-headingColor font-[500] text-[16px] leading-7'>
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur facilis laudantium aut quas enim! Molestiae, 
+                amet architecto? Obcaecati, cumque dolore. Optio iste commodi blanditiis 
+                aliquam obcaecati, similique impedit quia doloribus.
+            </p>
+        </div>
+        <div className='flex flex-col justify-center sm:py-12'>
+            <div className='w-full py-3 px-2 sm:max-w-xl sm:mx-auto sm:px-0'>
+                <div className='relative text-gray-700 antialiased text-sm font-semibold'>
+
+                    {/* vertical line running through the middle */}
+                    <div className="hidden absolute w-1 sm:block bg-indigo-300 h-full left-1/2 transform-translate-x-1/2">
+
+                    </div>
+
+                    {/* Left card */}
+                    <div className='mt-6 sm:mt-0 sm:mb-12'>
+                        <div className='flex items-center flex-col sm:flex-row'>
+
+                            <div className='flex justify-start w-full mx-auto items-center'>
+                            <div className='w-full sm:w-1/2 sm:pr-8'>
+                                <div 
+                                data-aos='fade-right'
+                                 data-aos-duration='1200'
+                                 className='bg-white p-4 rounded shadow group hover:bg-primaryColor cusor-pointer ease-in duration-150'>
+                                    <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                        Frontend Developer
+                                    </h3>
+                                    <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7'>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                        Eos quisquam in animi architecto quia rerum modi harum.
+                                    </p>
+
+                                </div>
+                                </div>
+                                </div>
+
+                                <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                    <figure>
+                                    <i class="ri-git-repository-line"></i>
+                                    </figure>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+{/* end of left card */}
+                        {/*start of Right card */}
+                        <div className='mt-6 sm:mt-0 sm:mb-12'>
+                        <div className='flex items-center flex-col sm:flex-row'>
+
+                            <div className='flex justify-end w-full mx-auto items-center'>
+                            <div className='w-full sm:w-1/2 sm:pl-8'>
+                                <div 
+                                data-aos='fade-left'
+                                 data-aos-duration='1200'
+                                 className='bg-white p-4 rounded shadow group hover:bg-primaryColor cusor-pointer ease-in duration-150'>
+                                    <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                        Frontend Developer
+                                    </h3>
+                                    <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7'>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                        Eos quisquam in animi architecto quia rerum modi harum.
+                                    </p>
+
+                                </div>
+                                </div>
+                                </div>
+
+                                <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                   transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                    <figure>
+                                    <i class="ri-git-repository-line"></i>
+                                    </figure>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        {/* end of right card */}
+
+                        {/* left card backend */}
+                        <div className='mt-6 sm:mt-0 sm:mb-12'>
+                        <div className='flex items-center flex-col sm:flex-row'>
+
+                            <div className='flex justify-start w-full mx-auto items-center'>
+                            <div className='w-full sm:w-1/2 sm:pr-8'>
+                                <div 
+                                data-aos='fade-right'
+                                 data-aos-duration='1200'
+                                 className='bg-white p-4 rounded shadow group hover:bg-primaryColor cusor-pointer ease-in duration-150'>
+                                    <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                        Frontend Developer
+                                    </h3>
+                                    <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7'>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                        Eos quisquam in animi architecto quia rerum modi harum.
+                                    </p>
+
+                                </div>
+                                </div>
+                                </div>
+
+                                <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                    <figure>
+                                    <i class="ri-git-repository-line"></i>
+                                    </figure>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        {/* right card design */}
+
+                        <div className='mt-6 sm:mt-0 sm:mb-12'>
+                        <div className='flex items-center flex-col sm:flex-row'>
+
+                            <div className='flex justify-end w-full mx-auto items-center'>
+                            <div className='w-full sm:w-1/2 sm:pl-8'>
+                                <div 
+                                data-aos='fade-left'
+                                 data-aos-duration='1200'
+                                 className='bg-white p-4 rounded shadow group hover:bg-primaryColor cusor-pointer ease-in duration-150'>
+                                    <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                        Frontend Developer
+                                    </h3>
+                                    <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7'>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                        Eos quisquam in animi architecto quia rerum modi harum.
+                                    </p>
+
+                                </div>
+                                </div>
+                                </div>
+
+                                <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                   transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                    <figure>
+                                    <i class="ri-git-repository-line"></i>
+                                    </figure>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        {/* end of cards */}
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+   </section>
+  )
+}
+
+export default Services
