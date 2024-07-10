fc09004a1096dcd8857be33435beb68253d8f655
dillahmwangi <mcharity241@gmail.com>
2024-07-10 11:49:01 +0300

    import portfolio data

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import portfolios from '../../assets/data/portfolioData'
 
-const Modal = () => {
+const Modal = ({activeID, setShowModal}) => {
+
+  const portfolio = portfolios.find(portfolios => portfolio.id = activeID)
   return (
     <div>Modal</div>
   )
