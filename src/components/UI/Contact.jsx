0df36735b872e86c2e4de3e823c3755edbe19ed9
dillahmwangi <mcharity241@gmail.com>
2024-07-11 12:22:20 +0300

    created contact.jsx file

---
+++ src/components/UI/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section id='contact' className='pb-16'>
+        <div className='container'>
+            <div className='md:flex justify-between items-center'>
+                <div className='w-full md:w-1/2 h-[30px] sm:h-[450px]'>
+                <iframe src="" frameborder="0"></iframe>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Contact
