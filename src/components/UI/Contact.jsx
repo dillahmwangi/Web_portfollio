7205d0f965101e252f73a5f4631ed3bf89346609
dillahmwangi <mcharity241@gmail.com>
2024-07-11 12:25:44 +0300

    included map link

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <section id='contact' className='pb-16'>
         <div className='container'>
             <div className='md:flex justify-between items-center'>
-                <div className='w-full md:w-1/2 h-[30px] sm:h-[450px]'>
-                <iframe src="" frameborder="0"></iframe>
+                <div className='w-full md:w-1/2 h-[300px] sm:h-[450px]'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.454435593905!2d36.84915294003955!3d-1.2534464797536709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17eb1d447363%3A0x17a2d29bdcf01fda!2sKCA%20University!5e0!3m2!1sen!2ske!4v1720689887965!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
                 </div>
 
