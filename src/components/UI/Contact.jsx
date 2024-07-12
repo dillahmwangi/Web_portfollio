eb6a7e1e1fb5ba05ba356366cd34269541557207
dillahmwangi <mcharity241@gmail.com>
2024-07-12 11:52:20 +0300

    created contact section

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -2,13 +2,47 @@ import React from 'react'
 
 const Contact = () => {
   return (
+
     <section id='contact' className='pb-16'>
         <div className='container'>
             <div className='md:flex justify-between items-center'>
                 <div className='w-full md:w-1/2 h-[300px] sm:h-[450px]'>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.454435593905!2d36.84915294003955!3d-1.2534464797536709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17eb1d447363%3A0x17a2d29bdcf01fda!2sKCA%20University!5e0!3m2!1sen!2ske!4v1720689887965!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe title = 'google-maps'
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.
+                454566306433!2d36.849152940240515!3d-1.2534250270553549!2m3!1f0!2f0!3f0!3m2!
+                1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17eb1d447363%3A0x17a2d29bdcf01fda!2sKCA%
+                20University!5e0!3m2!1sen!2ske!4v1720765635686!5m2!1sen!2ske" 
+                className='border-0 w-full h-full'
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade">
+                </iframe>
+              </div>
+              <div className='w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] 
+              lg:flex items-center bg-indigo-100 px-4 lg:px-8'>
 
-                </div>
+                <form className="w-full">
+                  <div className='mb-5'>
+                    <input type="text"
+                          placeholder='Enter Your Name'
+                          className='w-full p-3 focus:outline-none rounded-[5px]'/>
+                  </div>
+
+                  <div className='mb-5'>
+                    <input type="email"
+                          placeholder='Enter Your Email'
+                          className='w-full p-3 focus:outline-none rounded-[5px]'/>
+                  </div>
+
+                  <div className='mb-5'>
+                    <input type="text"
+                          placeholder='Write Subject'
+                          className='w-full p-3 focus:outline-none rounded-[5px]'/>
+                  </div>
+
+                </form>
+
+              </div>
 
             </div>
 
